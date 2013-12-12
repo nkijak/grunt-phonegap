0c1f528de432a5a76e39e0ed4a093e6dd49e1b0f
nkijak <nkijak@chariotsolutions.com>
2013-12-12 12:31:34 -0500

    Forgot to add new files

---
+++ src/tasks/phonegapCmd.coffee
@@ -0,0 +1,4 @@
+determineCommand = ->
+  "cordova"
+
+exports.cmd = determineCommand()
---
+++ tasks/phonegapCmd.js
@@ -0,0 +1,10 @@
+(function() {
+  var determineCommand;
+
+  determineCommand = function() {
+    return "cordova";
+  };
+
+  exports.cmd = determineCommand();
+
+}).call(this);
